b9de772c9ed41c3d0a122e7e96443381403e1672
Ulysse Carion <ulysse@ulysse.io>
2017-12-04 23:58:06 -0800

    More refactor: terrain works

--- src/render/mod.rs
+++ src/render/mod.rs
@@ -1,30 +1,52 @@
+use std::thread;
+use std::sync::mpsc;
+
 use cgmath::{Matrix4, Vector2};
 use gfx;
 use lru_cache::LruCache;
 
-use errors::*;
-
 mod terrain;
 mod polygon;
 
-use asset_getter::TileAssets;
-use self::terrain::TerrainRenderer;
+use asset_getter::{TileAssets, TileAssetData};
+use errors::*;
 use self::polygon::PolygonRenderer;
+use self::terrain::TerrainRenderer;
 use tile::Tile;
 use tile_chooser;
+use tile_fetcher;
 
 pub struct Renderer<R: gfx::Resources, F: gfx::Factory<R>> {
     asset_cache: LruCache<Tile, TileAssets<R>>,
-    terrain_renderer: TerrainRenderer<R, F>,
+    factory: F,
     polygon_renderer: PolygonRenderer<R, F>,
+    terrain_renderer: TerrainRenderer<R, F>,
+    texture_receiver: mpsc::Receiver<(Tile, Result<TileAssetData>)>,
+    tile_sender: mpsc::Sender<Tile>,
 }
 
 impl<R: gfx::Resources, F: gfx::Factory<R> + Clone> Renderer<R, F> {
     pub fn new(factory: F) -> Result<Renderer<R, F>> {
+        let (tile_sender, tile_receiver) = mpsc::channel();
+        let (texture_sender, texture_receiver) = mpsc::channel();
+
+        let polygon_renderer = PolygonRenderer::new(factory.clone())?;
+        let terrain_renderer = TerrainRenderer::new(factory.clone())?;
+
+        thread::Builder::new()
+            .name("tile_fetcher".to_string())
+            .spawn(move || {
+                tile_fetcher::fetch_tiles(tile_receiver, texture_sender)
+            })
+            .chain_err(|| "Error creating texture loader thread")?;
+
         Ok(Renderer {
             asset_cache: LruCache::new(512),
-            terrain_renderer: TerrainRenderer::new(factory.clone())?,
-            polygon_renderer: PolygonRenderer::new(factory.clone())?,
+            factory,
+            polygon_renderer,
+            terrain_renderer,
+            texture_receiver,
+            tile_sender,
         })
     }
 
@@ -41,8 +63,34 @@ impl<R: gfx::Resources, F: gfx::Factory<R> + Clone> Renderer<R, F> {
         look_at: Vector,
         camera_height: f32,
     ) -> Result<()> {
+        // Get tiles loaded in background thread, and put them in the cache
+        for (tile, tile_texture_data) in self.texture_receiver.try_iter() {
+            let assets = tile_texture_data?.create_assets(&mut self.factory)?;
+            self.asset_cache.insert(tile, assets);
+        }
+
+        let mvp = mvp.into();
+
         let (level_of_detail, tiles_to_render, tiles_to_fetch) =
-            tile_chooser::choose_tiles(&mut self.asset_cache, mvp.into(), look_at.into(), camera_height);
+            tile_chooser::choose_tiles(&mut self.asset_cache, mvp.clone(), look_at.into(), camera_height);
+
+        for tile_to_fetch in tiles_to_fetch {
+            self.tile_sender.send(tile_to_fetch).chain_err(|| "Error sending tile to background thread")?;
+        }
+
+        for (positioned_tile, indices) in tiles_to_render {
+            let tile_assets = self.asset_cache.get_mut(&positioned_tile.tile).unwrap();
+
+            self.terrain_renderer.render(
+                encoder,
+                target.clone(),
+                stencil.clone(),
+                &mvp,
+                positioned_tile,
+                indices,
+                tile_assets,
+            );
+        }
 
         Ok(())
     }
--- src/render/terrain.rs
+++ src/render/terrain.rs
@@ -1,7 +1,11 @@
+use cgmath::{Matrix4, Vector2};
 use gfx;
 use gfx::traits::FactoryExt;
 
+use asset_getter::TileAssets;
+use constants::ELEVATION_TILE_WIDTH;
 use errors::*;
+use tile::PositionedTile;
 
 #[cfg_attr(rustfmt, rustfmt_skip)]
 gfx_vertex_struct!(Vertex {
@@ -18,12 +22,21 @@ gfx_pipeline!(pipe {
 });
 
 pub struct TerrainRenderer<R: gfx::Resources, F: gfx::Factory<R>> {
-    pso: gfx::PipelineState<R, pipe::Meta>,
     factory: F,
+    pso: gfx::PipelineState<R, pipe::Meta>,
+    sampler: gfx::handle::Sampler<R>,
+    vertex_buffer: gfx::handle::Buffer<R, Vertex>,
 }
 
 impl<R: gfx::Resources, F: gfx::Factory<R>> TerrainRenderer<R, F> {
     pub fn new(mut factory: F) -> Result<TerrainRenderer<R, F>> {
+        let sampler = factory.create_sampler(gfx::texture::SamplerInfo::new(
+            gfx::texture::FilterMethod::Bilinear,
+            gfx::texture::WrapMode::Clamp,
+        ));
+
+        let vertex_buffer = Self::create_vertex_buffer(&mut factory);
+
         let pso = factory
             .create_pipeline_simple(
                 include_bytes!("../shaders/terrain.glslv"),
@@ -32,6 +45,66 @@ impl<R: gfx::Resources, F: gfx::Factory<R>> TerrainRenderer<R, F> {
             )
             .chain_err(|| "Could not create pipeline")?;
 
-        Ok(TerrainRenderer { factory, pso })
+        Ok(TerrainRenderer {
+            factory,
+            pso,
+            sampler,
+            vertex_buffer,
+        })
+    }
+
+    pub fn render<
+        C: gfx::CommandBuffer<R>,
+    >(
+        &mut self,
+        encoder: &mut gfx::Encoder<R, C>,
+        target: gfx::handle::RenderTargetView<R, gfx::format::Srgba8>,
+        stencil: gfx::handle::DepthStencilView<R, gfx::format::DepthStencil>,
+        mvp: &Matrix4<f32>,
+        positioned_tile: PositionedTile,
+        indices: Vec<u16>,
+        tile_assets: &TileAssets<R>,
+    ) {
+        let slice = gfx::Slice {
+            start: 0,
+            end: indices.len() as u32,
+            base_vertex: 0,
+            instances: None,
+            buffer: self.factory.create_index_buffer(&indices[..]),
+        };
+
+        let offset = Matrix4::from_translation(positioned_tile.offset().into());
+        let scale = Matrix4::from_nonuniform_scale(
+            positioned_tile.tile.width(),
+            -positioned_tile.tile.width(),
+            1.0,
+        );
+
+        let mvp = mvp * offset * scale;
+
+        let data = pipe::Data {
+            o_color: target.clone(),
+            o_depth: stencil.clone(),
+            t_color: (tile_assets.color.clone(), self.sampler.clone()),
+            t_elevation: (tile_assets.elevation.clone(), self.sampler.clone()),
+            u_mvp: mvp.into(),
+            vertex_buffer: self.vertex_buffer.clone(),
+        };
+
+        encoder.draw(&slice, &self.pso, &data);
+    }
+
+    fn create_vertex_buffer(factory: &mut F) -> gfx::handle::Buffer<R, Vertex> {
+        let mut vertex_data = vec![];
+        for y in 0..ELEVATION_TILE_WIDTH {
+            for x in 0..ELEVATION_TILE_WIDTH {
+                let coord_scale = (ELEVATION_TILE_WIDTH - 1) as f32;
+                vertex_data.push(Vertex {
+                    coord: [x as f32 / coord_scale, y as f32 / coord_scale],
+                });
+            }
+        }
+
+        factory.create_vertex_buffer(&vertex_data)
     }
 }
